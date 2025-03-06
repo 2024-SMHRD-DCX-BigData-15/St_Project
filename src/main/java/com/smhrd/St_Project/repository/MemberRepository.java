02b4de5e105166e0b97210371d12f2f24956e7b6
BaeChangHwan <bksm12@naver.com>
2025-03-06 11:23:27 +0900

    MemberRepository, TankService

--- src/main/java/com/smhrd/St_Project/repository/MemberRepository.java
+++ src/main/java/com/smhrd/St_Project/repository/MemberRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.smhrd.St_Project.entity.MemberEntity;
 
-public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+public interface MemberRepository extends JpaRepository<MemberEntity, String> {
 	
 	boolean existsById(String id);
 	
