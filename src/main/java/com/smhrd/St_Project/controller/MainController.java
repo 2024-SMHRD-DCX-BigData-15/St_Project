d29d8784e9682503738e25d95c6e30b97a94c0ec
LimYuJi <znzn40452@gmail.com>
2025-03-10 11:04:20 +0900

    maincontroller

--- src/main/java/com/smhrd/St_Project/controller/MainController.java
+++ src/main/java/com/smhrd/St_Project/controller/MainController.java
@@ -2,6 +2,7 @@ package com.smhrd.St_Project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -17,11 +18,11 @@ public class MainController {
 		return "register";
 	}
 	
-	// 메인 대시보드 페이지 이동
-	@GetMapping("/maindashboard")
+	// 회원가입 페이지 이동
+	@PostMapping("/maindashboard")
 	public String maindashboard() {
 		return "maindashboard";
 	}
-
+		
 
 }
