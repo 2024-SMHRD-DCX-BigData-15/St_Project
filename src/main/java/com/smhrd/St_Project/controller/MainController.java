262eaf60f6eb36a16e2ccac585946cbdad94f578
LimYuJi <znzn40452@gmail.com>
2025-03-10 14:02:39 +0900

    dashboarddetail commit

--- src/main/java/com/smhrd/St_Project/controller/MainController.java
+++ src/main/java/com/smhrd/St_Project/controller/MainController.java
@@ -33,6 +33,18 @@ public class MainController {
 	    }
 	    return "maindashboard";  // maindashboard.html로 반환
 	}
+	
+	// 메인대시보드 페이지 이동
+		@GetMapping("/dashboarddetail")
+		public String showDashboarddetail(HttpSession session) {
+		    MemberEntity loginUser = (MemberEntity) session.getAttribute("loginUser");
+		    if (loginUser != null) {
+		        System.out.println("Login User: " + loginUser.getUserName());  // 로그인한 사용자의 이름 출력
+		    } else {
+		        System.out.println("No user is logged in.");
+		    }
+		    return "dashboarddetail";  // maindashboard.html로 반환
+		}
 
 	@GetMapping("/login")
 	public String loginPage() {
